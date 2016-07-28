59a6a5e8d9f1365faa1bc3c24804778e35c26e1c
LeMuecke email = simonklimaschka@t-online.de <lemuecke@le>
2016-07-29 00:12:18 +0200

    Changed location in Configurator (to test committing)

--- src/Configurator.java
+++ src/Configurator.java
@@ -5,6 +5,6 @@ public final class Configurator {
 
     public static final double LATITUDE = 48.0;
     public static final double LONGITUDE = 10.0;
-    public static final String FILE_LOCATION = "C:\\Users\\***\\Desktop\\pokemon.xml";
+    public static final String FILE_LOCATION = "C:\\pokemon.xml";
 
 }
