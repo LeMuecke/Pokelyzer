7fba53ad5177654ceb610adc332a27008677c5b2
lemuecke <lemuecke@le>
2016-07-29 00:00:35 +0200

    Added Configurator

---
+++ src/Configurator.java
@@ -0,0 +1,10 @@
+/**
+ * Created by simon on 28.07.2016.
+ */
+public final class Configurator {
+
+    public static final double LATITUDE = 48.0;
+    public static final double LONGITUDE = 10.0;
+    public static final String FILE_LOCATION = "C:\\Users\\***\\Desktop\\pokemon.xml";
+
+}
